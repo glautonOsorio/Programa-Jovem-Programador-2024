45cfe5dc53206b05774da773a33d0761845845ab
GLAUTON OSORIO <glauton.osorio@alunos.sc.senac.br>
2024-03-28 16:49:58 -0300

    feat: terminando Programa03 + iniciando Programa04

--- Aula01/src/exemplos/Programa03.java
+++ Aula01/src/exemplos/Programa03.java
@@ -3,8 +3,64 @@ package exemplos;
 public class Programa03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+
+		String texto = "Good Bye Underworld";
+		
+		int comprimento = texto.length();
+		
+		System.out.println(comprimento);
+		
+		
+		String novoText = texto.replace(texto, "Até mais mundo inferior");
+		
+		System.out.println(novoText);
+		
+    	String subString1 = texto.substring(4);
+		System.out.println(subString1);
+		
+		String subString2 = texto.substring(4,9);
+		System.out.println(subString2);
+		
+		String subString3 = texto.substring(0,4);
+		System.out.println(subString3);
+		
+		
+		char letra = texto.charAt(5);
+		
+		System.out.println(letra);
+		
+		String testeMaisculo = "UNDEAD";
+		
+		System.out.println(testeMaisculo.toLowerCase());
+		
+		String testeMinusculo = "unluck";
+		
+		System.out.println(testeMinusculo.toUpperCase());
+		
+		
+		String testeTrim = "                           SpyXFamily";
+		
+		System.out.println(testeTrim.trim());
+		
+		
+		
+		String senha = "123456";
+		String tentativa = "1234";
+		boolean teste = senha.equals(tentativa);
+		
+		
+		System.out.println(teste);
+		
+		
+		
+		String frase2 = "Fullstack programmer";
+		boolean contain = frase2.contains("Fullstack");
+		
+		System.out.println(contain);
+					
+		
+		
 	}
 
 }
