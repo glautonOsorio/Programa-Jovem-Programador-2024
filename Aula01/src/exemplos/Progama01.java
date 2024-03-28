17feb3646adb349356f35f8a4365cfe338b12d3e
GLAUTON OSORIO <glauton.osorio@alunos.sc.senac.br>
2024-03-28 14:52:46 -0300

    feat: first commit

---
+++ Aula01/src/exemplos/Progama01.java
@@ -0,0 +1,29 @@
+package exemplos;
+
+public class Progama01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World " + 5 + " Coisa coisada");
+		
+		String nome = "Thiago";
+		
+		System.out.println("Hello World " + nome + " Coisa coisada");
+		nome = "Roberto";
+	
+		System.out.println(nome);
+		
+		int idade = 50;
+		String idade2 = "80";
+		Integer valor = 30;
+		idade = Integer.parseInt(idade2);
+		
+	System.out.println(nome + " tem " + idade + " anos");
+	
+	double preco = 2.99;
+	double salario = 500;
+	
+	}
+
+}
