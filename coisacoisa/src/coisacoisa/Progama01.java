cd23ebec66e5e86e4710b56663bc542963c8b678
Glauton Osório <glautonleao@gmail.com>
2024-04-10 06:20:53 -0300

    feat: teste

---
+++ coisacoisa/src/coisacoisa/Progama01.java
@@ -0,0 +1,43 @@
+package coisacoisa;
+
+public class Progama01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World " + 5 + " Coisa coisada");
+		
+		String nome = "Thiago";
+		
+		System.out.println("Hello World " + nome + " Coisa coisada");
+		nome = "Roberto";
+	
+		System.out.println(nome);
+		
+		int idade = 50;
+		String idade2 = "80";
+		Integer valor = 30;
+		idade = Integer.parseInt(idade2);
+		
+	System.out.println(nome + " tem " + idade + " anos");
+	
+	double preco = 2.99;
+	double salario = 500;
+	
+	boolean verdade = true;
+	verdade = false;
+	
+	
+	char letra = 'A';
+	
+	char outraLetra = 101;
+	
+	String nomeTeste = "Garcia";
+	
+	char primeiraLetraNome = nomeTeste.charAt(3);
+	
+	System.out.println("A primeira letra é: " + primeiraLetraNome);
+	
+	}
+
+}
---
+++ coisacoisa/src/coisacoisa/Programa02.java
@@ -0,0 +1,37 @@
+package coisacoisa;
+
+public class Programa02 {
+
+	public static void main(String[] args) {
+
+		
+		double preco = 1234.567891;
+		
+	 System.out.println(preco);
+	 
+	 System.out.printf("Valor total: %.2f%n",preco);
+	 
+	 String produto = "Teclado";
+	 int quantidade = 10;
+	 
+	 System.out.printf("produto comprado: %s%n:",produto);
+	 System.out.printf("Quatidade de %s comprados %d:%n: ", produto,quantidade);
+			 
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
