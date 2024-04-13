b2dc317976a58b2b1183418ff35452cbe4dca456
Glauton Osório <glautonleao@gmail.com>
2024-04-13 17:54:04 -0300

    feat: arrumando erro no desafio04

--- coisacoisa/src/desafios/Desafio04.java
+++ coisacoisa/src/desafios/Desafio04.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Desafio04 {
 
     public static void main(String[] args) {
-
         Scanner read = new Scanner(System.in);
 
         System.out.println("Digite sua renda: Ex.1550,15");
@@ -27,45 +26,31 @@ public class Desafio04 {
         String discountFormat;
 
         if (num >= 2001.01 && num <= 3000.00) {
-
             taxEvade = num - tax;
             discount = (taxEvade / 100) * 8;
-
             discountFormat = String.format("%.2f", discount);
-
             System.out.printf("O seu imposto de renda é R$%s", discountFormat);
-
         } else if (num >= 3001.01 && num <= 4500.00) {
-
             taxEvade = (num - tax);
             discount = (taxEvade / 100) * 8;
             taxEvade2 = taxEvade - tax2;
             discount2 = (taxEvade2 / 100) * 18;
-            finalValue =  discount + discount2;
-
+            finalValue = discount + discount2;
             discountFormat = String.format("%.2f", finalValue);
-
             System.out.printf("O seu imposto de renda é R$%s", discountFormat);
-
         } else if (num >= 4501.00) {
-
             taxEvade = (num - tax);
-            taxEvade2 = taxEvade - tax2;
-            taxEvade3 = taxEvade2 - tax3;
-            discount = (taxEvade / 8) ;
-            discount2 = (taxEvade2 / 18) ;
-            discount3 = (taxEvade3 / 28) ;
-            finalValue =  discount + discount2 + discount3;
-
-
+            taxEvade2 = num - (tax + tax2);
+            taxEvade3 = num - (tax + tax2 + tax3);
+            discount = (taxEvade / 100) * 8;
+            discount2 = (taxEvade2 / 100) * 18;
+            discount3 = (taxEvade3 / 100) * 28;
+            finalValue = discount + discount2 + discount3;
             discountFormat = String.format("%.2f", finalValue);
-
             System.out.printf("O seu imposto de renda é R$%s", discountFormat);
-
         } else {
-            System.out.println("Você esta isento de imposto");
+            System.out.println("Você está isento de imposto");
         }
-
     }
 
 }
