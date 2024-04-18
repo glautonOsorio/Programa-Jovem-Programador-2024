bc1f2cbae8750152ccac486cba4a88f73e411829
GLAUTON OSORIO <glauton.osorio@alunos.sc.senac.br>
2024-04-18 17:03:59 -0300

    feat: jogo forca

---
+++ Aula03/src/exemplos/JogoForcar.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class JogoForcar {
+
+    public static void main(String[] args) {
+
+        Scanner read = new Scanner(System.in);
+
+        String secretWord = "programmer";
+
+        StringBuilder word = new StringBuilder("__________");
+
+        int maxAttempts = 6;
+        int attempts = 0;
+        boolean rightWord = false;
+
+        while (true) {
+
+            System.out.println(word);
+            System.out.println("Digite uma letra para descobrir qual é a palavra:");
+            char letter = read.nextLine().charAt(0);
+
+            for (int i = 0; i < secretWord.length(); i++) {
+
+                if ( letter == secretWord.charAt(i)) {
+
+                    word.setCharAt(i, letter);
+                    rightWord = true;
+
+                } 
+                if(letter != secretWord.charAt(i)) {
+                    attempts++;
+                    rightWord = false;
+                    System.out.println("Você errou a palavra");
+
+                }
+            }
+            if (attempts == maxAttempts) {
+                System.out.println("Você gastou todas as tentativas");
+                break;
+               
+            }
+
+            if (rightWord == true) {
+                System.out.println("Você acertou uma palvra");
+            }
+            if (secretWord.equals(word.toString())) {
+
+                System.out.println("Parabens você ganhou uma batata");
+                return;
+                
+            }
+
+
+
+        }
+        read.close();
+
+    }
+
+}
---
+++ Aula03/src/exemplos/Programa10.java
@@ -0,0 +1,15 @@
+public class Programa10 {
+
+    public static void main(String[] args) {
+
+        String word = "Programmer";
+
+        for (int i = 0; i < word.length(); i++) {
+
+            System.out.println(word.charAt(i));
+
+        }
+
+    }
+
+}
